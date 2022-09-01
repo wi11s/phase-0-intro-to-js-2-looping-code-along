90c8fb3d3681b779e947078f0046536388d39bff
William <wms274@nyu.edu>
2022-09-01 10:46:29 -0400

    commit tree 83755ee5572140d5ea216ac4f066eafbae756926

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// Code your solutions in this file
+// const gifts = ["teddy bear", "drone", "doll"];
+
+// function wrapGifts(gifts) {
+//     for (let i = 0; i < gifts.length; i++) {
+//         console.log(`Wrapped ${gifts[i]} and added a bow!`);
+//     }
+//     return gifts;
+// }
+
+// wrapGifts(gifts);
+
+function writeCards(arrayOfNames, eventName) {
+    let arrayOfMessages = []
+    for (let i = 0; i < arrayOfNames.length; i++) {
+        arrayOfMessages[i] = `Thank you, ${arrayOfNames[i]}, for the wonderful ${eventName} gift!`
+    }
+    return arrayOfMessages
+}
+
+const countDown = (number) => {
+    while (number >=0) {
+        console.log(number)
+        --number
+    }
+}
